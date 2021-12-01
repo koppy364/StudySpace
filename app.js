15c1dd508e54a5410008b3c5862b722214bf0d9a
KYS <koppy364@gmail.com>
2021-12-01 21:56:40 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+/*변수 
+const 안변함 let 변함 var 구분불가 사용X
+const   a = 5;
+const b = 2;
+let myName = "Kim";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " + myName);
+
+myName = "KimYoungseo"
+
+console.log("your new name is " + myName);
+ */
+
+/*배열
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sund";
+
+const daysOfWeek = ["mon", tue, wed, thu, fri, sat];
+
+console.log(daysOfWeek);
+
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+*/
+
+/*function
+function sayHello(nameOfPerson, age){
+    console.log("hello my name is " + nameOfPerson + " and I'm " + age);
+};
+
+//alert();
+sayHello("kim1", 10);
+sayHello("kim2", 20);
+sayHello("kim3", 30);
+
+function plus(firstNumber, secondNumber){
+    console.log(firstNumber + secondNumber);
+};
+
+function divide(a, b){
+    console.log(a/b);
+};
+plus(8, 60);
+divide(8, 60);
+*/
+
+/*object
+const player = {
+    name: "kim",
+    sayHello: function(otherPersonName){
+        console.log("hello! " + otherPersonName + " nice to meet you");
+    },
+};
+
+console.log(player.name);
+player.sayHello("Lee");
+player.sayHello("Park");
+*/
