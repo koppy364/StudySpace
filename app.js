3f44e5dbd5f3612df9e33d3cc3122f35014a02dc
KYS <koppy364@gmail.com>
2022-02-20 11:29:01 +0900

    js function, condition

--- app.js
+++ app.js
@@ -1,66 +1,13 @@
-/*변수 
-const 안변함 let 변함 var 구분불가 사용X
-const   a = 5;
-const b = 2;
-let myName = "Kim";
+const age = parseInt(prompt("how old are you?"));
 
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("hello " + myName);
+console.log(isNaN(age));
 
-myName = "KimYoungseo"
-
-console.log("your new name is " + myName);
- */
-
-/*배열
-const tue = "tue";
-const wed = "wed";
-const thu = "thu";
-const fri = "fri";
-const sat = "sat";
-const sun = "sund";
-
-const daysOfWeek = ["mon", tue, wed, thu, fri, sat];
-
-console.log(daysOfWeek);
-
-daysOfWeek.push("sun");
-
-console.log(daysOfWeek);
-*/
-
-/*function
-function sayHello(nameOfPerson, age){
-    console.log("hello my name is " + nameOfPerson + " and I'm " + age);
-};
-
-//alert();
-sayHello("kim1", 10);
-sayHello("kim2", 20);
-sayHello("kim3", 30);
-
-function plus(firstNumber, secondNumber){
-    console.log(firstNumber + secondNumber);
-};
-
-function divide(a, b){
-    console.log(a/b);
-};
-plus(8, 60);
-divide(8, 60);
-*/
-
-/*object
-const player = {
-    name: "kim",
-    sayHello: function(otherPersonName){
-        console.log("hello! " + otherPersonName + " nice to meet you");
-    },
-};
-
-console.log(player.name);
-player.sayHello("Lee");
-player.sayHello("Park");
-*/
+if(isNaN(age)){
+  console.log("Please write a number")
+} else if(age < 18){
+  console.log("You are too young.");
+} else if(age >= 18 && age <= 50){
+  console.log("You can drink");
+} else {
+  console.log("You can't drink");
+}
