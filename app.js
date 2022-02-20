d96b28f11ac235cd980b10601ee94d3a4f7b3016
KYS <koppy364@gmail.com>
2022-02-20 13:57:54 +0900

    js로 css제어하기

--- app.js
+++ app.js
@@ -1,40 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
-//event 검색방법 h1 element mdn or
-console.dir(h1);
+function handleTitleClick() {
+  // if(h1.classList.contains(clickedClass)){
+  //   h1.classList.remove(clickedClass);
+  // } else {
+  //   h1.classList.add(clickedClass);
+  // }
 
-function handleh1Click() {
-  h1.style.color = "blue";
+  h1.classList.toggle("clicked");
 }
 
-function handleMouseEnter() {
-  h1.innerText = "mouse is here!";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
-} 
-
-function handleWindowResize(){
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-  alert("copier!");
-}
-
-function handleWindowOffline(){
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-  alert("ALL GOOOD");
-}
-h1.addEventListener("click", handleh1Click);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize); 
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
+ 
