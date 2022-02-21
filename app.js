5916c44bf17658e481fc4fe23c32cf99206ea22c
KYS <koppy364@gmail.com>
2022-02-21 10:20:13 +0900

    Login 구현

--- app.js
+++ app.js
@@ -1,14 +1,56 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function handleTitleClick() {
-  // if(h1.classList.contains(clickedClass)){
-  //   h1.classList.remove(clickedClass);
-  // } else {
-  //   h1.classList.add(clickedClass);
-  // }
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-  h1.classList.toggle("clicked");
+/*버튼사용
+const loginButton= document.querySelector("#login-form button");*/
+
+/*링크 이벤트동작 막기
+const link = document.querySelector("a");*/
+ 
+function OnLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  /*string과 변수결합 `string ${변수}` 
+  greeting.innerText = "Hello " + username;*/
+  localStorage.setItem(USERNAME_KEY,username);
+  paintGreetings(username);
+
+  /*버튼사용시 유효성검증
+  if(username === "") {
+    alert("Please write your name");
+  } else if(username.length > 15) {
+    alert("Your name is too long");
+  }*/
 }
 
-h1.addEventListener("click", handleTitleClick);
- 
+function paintGreetings(username){
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${savedUsername}`;
+}
+/*이벤트 동작막기
+function handleLinkClick(event){
+  event.preventDefault();
+  console.dir(event);
+  alert("clicked!");
+}*/
+
+loginForm.addEventListener("submit", OnLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+}else{
+  paintGreetings(savedUsername);
+}
+/*이벤트 동작막기
+link.addEventListener("click", handleLinkClick);*/
+
+/*버튼이벤트
+loginButton.addEventListener("click", onLoginBtnClick)*/
+
