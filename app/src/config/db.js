f81e48924d2b45beaf6792161dc85644a77b948b
KYS <koppy364@gmail.com>
2022-06-09 10:09:25 +0900

    dotenv로 환경변수 등록

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "logindb.cvbgca0demho.ap-northeast-2.rds.amazonaws.com",
-  user: "admin",
-  password: "dudtj321",
-  database: "login_lecture",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PSWORD,
+  database: process.env.DB_DATABASE,
 })
 
 db.connect();
